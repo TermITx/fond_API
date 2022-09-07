31928ba05e142aa8c43253ad48a9f55e0ce18ddc
Karlo Lochert <karlo.lochert@gmail.com>
2022-09-07 12:23:37 +0200

    poziv skirpte s finfeatures i yahooa napravljeno

--- services.py
+++ services.py
@@ -1,10 +1,15 @@
 import subprocess
 import time
+import pandas as pd
+import yfinance as yf
+from yahoofinancials import YahooFinancials
+from datetime import datetime
 
 PATH = "C:/Users/Karlo/Desktop/"
 
 def call_R_script(name : str):
     subprocess.call ("Rscript --vanilla " + PATH + name +  ".R", shell=True)
+    return {"Status":"Success"}
 
 def get_position(strategy : str):
     if strategy == 'DM':
@@ -17,5 +22,20 @@ def get_position(strategy : str):
 
         pass
 
+def call_yahoo(tickers=None,date=None):
+    if tickers is None:
+        tickers = ['AAL','AAPL','AMD','AMZN','APA','BAC','BEN','BMY','BSX','C','CCL','CL','CLF','CMCSA','CSCO','CSX','CVX','DAL','DIS','ET','FCX','FTI','GM','HBAN','INTC','JPM','KO','LUMN','MRK','MRO','MSFT','MU','NEE','NFLX','NVDA','NYCB','OXY','PCG','PFE','PLUG','QCOM','RF','RIG','SIRI','SPY','SWN','T','TELL','UEC','VZ','WBA','WBD','WFC','WU','X','XOM']
+    if date is None:
+        date = datetime.today().strftime('%Y-%m-%d')
+
+    for name in tickers:
+        aapl_df = yf.download(name, 
+                            start=date, 
+                            #end='2021-01-01', 
+                            progress=True,
+        )
+        to_append = pd.read_csv(f'{name}.csv')
+        to_append.append(aapl_df[-1:])
+        to_append.to_csv(f"{name}.csv")
 if __name__ == "__main__":
     call_R_script("probar")
