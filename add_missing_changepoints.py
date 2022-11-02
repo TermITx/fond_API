2a68a7ff9ff0544b7ab2723d1017985071e57fe8
matej <matejvagic@gmail.com>
2022-11-02 07:03:20 +0000

    last

--- add_missing_changepoints.py
+++ add_missing_changepoints.py
@@ -1,10 +1,14 @@
-
+from services import call_yahoo
 import subprocess
 if __name__ == "__main__":
     #call_yahoo_from_start()
     #call_yahoo()
     #my_worker.main(21)
     command = ["python" ,  "-m" ,  "model.examples.my_worker" ,    "21"]
+    process = subprocess.Popen(command)
+    process.wait()
+    
+    command = ["python" ,  "-m" ,  "model.examples.my_worker" ,    "126"]
     process = subprocess.Popen(command)
     process.wait()
     ##call_R_script("probar")
